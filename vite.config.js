53f051fbd4c0432a962d40465e8fcb7447802762
Vibhaeo Mudia <VibhaeoM01>
2025-05-29 12:38:51 +0530

    changed

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        // additionalData: `@import "./src/styles/variables.scss";`
-      }
-    }
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.scss']
-  }
+   
 })
